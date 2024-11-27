beda69e79caff4ef53e3c844da4b8866cfc2ee0b
Taufik Herjanto <taufik.herjanto@live.com>
2024-11-27 17:40:02 +0700

    Add postman collection

--- database/db.go
+++ database/db.go
@@ -18,7 +18,11 @@ var DB *gorm.DB
 func Connect() {
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
 	)
 
 	var err error
